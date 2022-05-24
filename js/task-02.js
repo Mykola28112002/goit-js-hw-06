984d0112f4576af7f47db79ad09ec8ee6cdff9f5
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-05-24 14:22:20 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+
+
+
+
+
+for (const ing of ingredients) {
+  // Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+  const ingredientsli = document.createElement('li')
+  // Додасть елементу клас item.
+  ingredientsli.setAttribute('class', 'item')
+  // Додасть назву інгредієнта як його текстовий вміст.
+  ingredientsli.textContent = `${ing}`
+  console.log(ingredientsli)
+  // Після чого, вставить усі <li> за одну операцію у список ul.ingredients.
+  document.body.appendChild(ingredientsli)
+}
+ 
