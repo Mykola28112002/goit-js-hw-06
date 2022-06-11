e813b09e8f790b55f0c3d5986dae9fc18b909cd9
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-11 17:45:59 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,20 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-// Напиши скрипт, який для кожного елемента масиву ingredients:
-
 const ulIngredients = document.querySelector("ul");
+
+// ulIngredients.appendChild(cardsMarkup);
+
+const ulIngredientsEl = ingredients.map(ingredient => {
+  const ingredientsli = document.createElement("li")
+  ingredientsli.classList = "item"
+
+  const paragraphEl =  document.createElement("p")
+  paragraphEl.textContent = `${ingredient}`
+
+  ingredientsli.append(paragraphEl)
   
-  
-ingredients.forEach(element => {
-   // Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
-  const ingredientsli = document.createElement('li')
-  // Додасть елементу клас item.
-  ingredientsli.setAttribute('class', 'item')
-  // Додасть назву інгредієнта як його текстовий вміст.
-  ingredientsli.textContent = `${element}`
-  console.log(ingredientsli)
-  // Після чого, вставить усі <li> за одну операцію у список ul.ingredients.
-  ulIngredients.appendChild(ingredientsli)
-});
- 
+  return ingredientsli
+})
+
+ulIngredients.append(...ulIngredientsEl)
